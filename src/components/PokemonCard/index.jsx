4d216924ff1eca4f483ccb7c3568506dec6e872f
Eduardo Scheunemann <dudu.jefe@hotmail.com>
2023-09-14 18:10:41 -0300

    enviando projeto pro github

---
+++ src/components/PokemonCard/index.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Box, CardActionArea } from '@mui/material';
+import styles from '../../styles/PokemonCard.module.css'
+
+export default function PokemonCard({name, image, types}) {
+
+  const typeHandler = () => {
+    if(types[1]) {
+      return types[0].type.name + " | " + types[1].type.name
+    }
+    return types[0].type.name
+  }
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          className={styles.cardImage}
+          component="img"
+          image={image}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Box display="flex" justifyContent="space-between" alignItems="center">
+            <Typography gutterBottom variant="h5" component="div">
+              {name}
+            </Typography>
+            <Typography gutterBottom variant="caption" component="div">
+              {typeHandler()}
+            </Typography>
+          </Box>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
---
+++ src/components/Skeletons/index.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unreachable */
+import { Container, Skeleton } from "@mui/material"
+
+export const Skeletons = () => {
+
+    
+  return(
+  <Container maxWidth="xxl">
+       <Skeleton variant="rounded" width="100%" height={150} sx={{marginBottom: "1em"}} />
+       <Skeleton variant="rounded" width="100%" height={150} sx={{marginBottom: "1em"}} />
+       <Skeleton variant="rounded" width="100%" height={150} sx={{marginBottom: "1em"}} />
+       <Skeleton variant="rounded" width="100%" height={150} sx={{marginBottom: "1em"}} />
+       <Skeleton variant="rounded" width="100%" height={150} sx={{marginBottom: "1em"}} />
+  </Container>
+  ) 
+
+  
+}
